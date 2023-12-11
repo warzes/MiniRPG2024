75377d70f2927036217ac1ab3f31340293d0f602
warzes <warzes@mail.ru>
2023-12-11 13:35:11 +1000

    - добавлен glfw - тест работы glfw

---
+++ Project/3rdparty/glfw.c
@@ -0,0 +1,14 @@
+#if defined(_MSC_VER)
+#	pragma warning(push, 0)
+#	pragma warning(disable : 4255)
+#	pragma warning(disable : 4820)
+#	pragma warning(disable : 5045)
+#endif
+
+#define _CRT_SECURE_NO_WARNINGS
+#define _GLFW_IMPLEMENTATION
+#include "glfw.h"
+
+#if defined(_MSC_VER)
+#	pragma warning(pop)
+#endif
--- Project/Game/main.cpp
+++ Project/Game/main.cpp
@@ -1,3 +1,58 @@
-﻿int main()
+﻿#include "glfw.h"
+
+static GLFWwindow* window = NULL;
+static int swapAndPollInput();
+
+#if defined(_MSC_VER)
+#	pragma comment( lib, "OpenGL32.lib" )
+#endif
+
+static void run() 
 {
+	glClearColor(0.1, 0.4, 1.0, 0.0);
+	do 
+	{
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClear(GL_COLOR_BUFFER_BIT);
+	} while (swapAndPollInput());
+}
+
+static int swapAndPollInput()
+{
+	glfwSwapBuffers(window);
+	glfwPollEvents();
+	return glfwWindowShouldClose(window) == GLFW_FALSE;
+}
+
+static void glfwErrorCallback(int, const char* message)
+{
+	//fprintf(stderr, "%s\n", message);
+}
+
+static void glMessageCallback(GLenum, GLenum, GLuint, GLenum, GLsizei, const char* message, const void*) 
+{
+	glfwErrorCallback(0, message);
+}
+
+int main() 
+{
+	// GLFW + OpenGL init
+	glfwInit();
+	glfwSetErrorCallback(glfwErrorCallback);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+	glfwWindowHint(GLFW_CONTEXT_ROBUSTNESS, GLFW_LOSE_CONTEXT_ON_RESET);
+	window = glfwCreateWindow(1024, 768, "GL test app", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	//glDebugMessageCallback(glMessageCallback, NULL);
+	//glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_FALSE);
+	//glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_HIGH, 0, NULL, GL_TRUE);
+	//glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_MEDIUM, 0, NULL, GL_TRUE);
+
+	// Event loop
+	run();
+	// Cleanup
+	glfwTerminate();
+	return 0;
 }
